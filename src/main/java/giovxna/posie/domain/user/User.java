d691d6caa6cb24ad1210e2bccfb2a86ba26bd934
giovxna <gi.siqueira0202@gmail.com>
2024-06-11 15:57:49 -0300

    feat: criação da entidade usuário e repositories

---
+++ src/main/java/giovxna/posie/domain/user/User.java
@@ -0,0 +1,22 @@
+package giovxna.posie.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+}
---
+++ src/main/java/giovxna/posie/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package giovxna.posie.repositories;
+
+import giovxna.posie.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
