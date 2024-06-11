20d4754cc51b0683ac948c1f6a49c3c42164f185
giovxna <gi.siqueira0202@gmail.com>
2024-06-11 01:53:51 -0300

    :sparkle: criação do projeto

---
+++ src/main/java/giovxna/posie/PosieApplication.java
@@ -0,0 +1,13 @@
+package giovxna.posie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosieApplication.class, args);
+	}
+
+}
